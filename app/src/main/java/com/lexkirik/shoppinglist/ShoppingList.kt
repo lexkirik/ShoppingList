3e29143899345ff8bd9214649c53ab43fd863ba9
Aliaksei Kirykovich <lexkirik@mail.ru>
2025-04-01 00:09:49 +0300

    added ShoppingItemEditor

--- app/src/main/java/com/lexkirik/shoppinglist/ShoppingList.kt
+++ app/src/main/java/com/lexkirik/shoppinglist/ShoppingList.kt
@@ -1,6 +1,7 @@
 package com.lexkirik.shoppinglist
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -8,9 +9,11 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
@@ -58,7 +61,29 @@ fun ShoppingListApp() {
             modifier = Modifier.fillMaxSize().padding(16.dp)
         ) {
             items(shoppingItems) {
-                ShoppingListItem(it, {}, {})
+                item ->
+                if(item.isEditing){
+                    ShoppingItemEditor(
+                        item = item,
+                        onEditComplete = {
+                        editedName, editedQuantity ->
+                        shoppingItems = shoppingItems.map { it.copy(isEditing = false) }
+                        val editedItem = shoppingItems.find { it.id == item.id }
+                        editedItem?.let {
+                            it.name = editedName
+                            it.quantity = editedQuantity
+                        }
+                    })
+                } else {
+                    ShoppingListItem(
+                        item = item,
+                        onEditClick = {
+                        shoppingItems = shoppingItems.map { it.copy(isEditing = it.id==item.id) }
+                    },
+                        onDeleteClick = {
+                        shoppingItems = shoppingItems - item
+                    })
+                }
             }
         }
     }
@@ -116,6 +141,45 @@ fun ShoppingListApp() {
 }
 
 @Composable
+fun ShoppingItemEditor(
+    item: ShoppingItem,
+    onEditComplete: (String, Int) -> Unit
+) {
+    var editedName by remember { mutableStateOf(item.name) }
+    var editedQuantity by remember { mutableStateOf(item.quantity.toString()) }
+    var isEditing by remember { mutableStateOf(item.isEditing) }
+
+    Row(
+        modifier = Modifier.fillMaxWidth().background(Color.White).padding(8.dp),
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ) {
+        Column() {
+            BasicTextField(
+                value = editedName,
+                onValueChange = {editedName = it},
+                singleLine = true,
+                modifier = Modifier.wrapContentSize().padding(8.dp)
+            )
+            BasicTextField(
+                value = editedQuantity,
+                onValueChange = {editedQuantity = it},
+                singleLine = true,
+                modifier = Modifier.wrapContentSize().padding(8.dp)
+            )
+        }
+
+        Button(
+            onClick = {
+                isEditing = false
+                onEditComplete(editedName, editedQuantity.toIntOrNull() ?: 1)
+            }
+        ) {
+            Text("Save")
+        }
+    }
+}
+
+@Composable
 fun ShoppingListItem(
     item: ShoppingItem,
     onEditClick: () -> Unit,
@@ -125,7 +189,8 @@ fun ShoppingListItem(
         modifier = Modifier.padding(8.dp).fillMaxSize().border(
             border = BorderStroke(2.dp, Color(0XFF018786)),
             shape = RoundedCornerShape(20)
-        )
+        ),
+        horizontalArrangement = Arrangement.SpaceBetween
     ) {
         Text(text = item.name, modifier = Modifier.padding(8.dp))
         Text(text = "Quantity: ${item.quantity}", modifier = Modifier.padding(8.dp))
