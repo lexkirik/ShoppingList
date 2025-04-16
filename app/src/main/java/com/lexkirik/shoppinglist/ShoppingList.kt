5ea32d893a6b61aebf5e560d3f51bec54a0c2136
Aliaksei Kirykovich <lexkirik@mail.ru>
2025-04-16 22:05:31 +0300

    added permissions, GeocodingApiService interface

--- app/src/main/java/com/lexkirik/shoppinglist/ShoppingList.kt
+++ app/src/main/java/com/lexkirik/shoppinglist/ShoppingList.kt
@@ -1,5 +1,10 @@
 package com.lexkirik.shoppinglist
 
+import android.Manifest
+import android.content.Context
+import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -17,6 +22,7 @@ import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
@@ -32,20 +38,55 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
+import androidx.navigation.NavController
+import java.nio.file.WatchEvent
 
 data class ShoppingItem(
     val id: Int,
     var name: String,
     var quantity: Int,
-    var isEditing: Boolean = false
+    var isEditing: Boolean = false,
+    var address: String = ""
 )
 
 @Composable
-fun ShoppingListApp() {
+fun ShoppingListApp(
+    locationUtils: LocationUtils,
+    viewModel: LocationViewModel,
+    navController: NavController,
+    context: Context,
+    address: String
+) {
     var shoppingItems by remember { mutableStateOf(listOf<ShoppingItem>()) }
     var showDialog by remember { mutableStateOf(false) }
     var itemName by remember { mutableStateOf("") }
     var itemQuantity by remember { mutableStateOf("") }
+
+    val requestPermissionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestMultiplePermissions(),
+        onResult = {permissions ->
+            if(permissions[android.Manifest.permission.ACCESS_COARSE_LOCATION] == true
+                && permissions[android.Manifest.permission.ACCESS_FINE_LOCATION] == true) {
+                locationUtils.requestLocationUpdates(viewModel = viewModel)
+            } else {
+                val rationaleReqired = ActivityCompat.shouldShowRequestPermissionRationale(
+                    context as MainActivity,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION
+                ) || ActivityCompat.shouldShowRequestPermissionRationale(
+                    context as MainActivity,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+
+                if(rationaleReqired) {
+                    Toast.makeText(context,
+                        "Location permission", Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(context,
+                        "Change permission in the settings", Toast.LENGTH_LONG).show()
+                }
+            }
+        })
 
     Column(
         modifier = Modifier.fillMaxSize(),
@@ -135,6 +176,21 @@ fun ShoppingListApp() {
                         singleLine = true,
                         modifier = Modifier.fillMaxWidth().padding(8.dp)
                     )
+                    Button(onClick = {
+                        if(locationUtils.hasLocationPermission(context)){
+                            locationUtils.requestLocationUpdates(viewModel)
+                            navController.navigate("locationscreen"){
+                                this.launchSingleTop
+                            }
+                        } else {
+                            requestPermissionLauncher.launch(arrayOf(
+                                Manifest.permission.ACCESS_FINE_LOCATION,
+                                Manifest.permission.ACCESS_COARSE_LOCATION
+                            ))
+                        }
+                    }) {
+                        Text("Adrress")
+                    }
                 }
             })
     }
@@ -192,8 +248,18 @@ fun ShoppingListItem(
         ),
         horizontalArrangement = Arrangement.SpaceBetween
     ) {
-        Text(text = item.name, modifier = Modifier.padding(8.dp))
-        Text(text = "Quantity: ${item.quantity}", modifier = Modifier.padding(8.dp))
+        Column(modifier = Modifier.weight(1f).padding(8.dp)) {
+            Row {
+                Text(text = item.name, modifier = Modifier.padding(8.dp))
+                Text(text = "Quantity: ${item.quantity}", modifier = Modifier.padding(8.dp))
+            }
+
+            Row(modifier = Modifier.fillMaxWidth()) {
+                Icon(imageVector = Icons.Default.LocationOn, contentDescription = null)
+                Text(text = item.address)
+            }
+        }
+
         Row(modifier = Modifier.padding(8.dp)) {
             IconButton(onClick = onEditClick) {
                 Icon(imageVector = Icons.Default.Edit, contentDescription = null)
